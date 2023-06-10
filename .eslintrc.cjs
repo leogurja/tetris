3c0a1ef496a391d6fb313767abd378c342abc393
Leonardo Gurgel <lgurgel@id.uff.br>
2023-06-10 15:00:09 -0300

    fix react eslint warning

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,5 +1,10 @@
 // eslint-disable-next-line no-undef
 module.exports = {
+  settings: {
+    react: {
+      version: "detect",
+    },
+  },
   env: {
     browser: true,
     es2021: true,
@@ -8,15 +13,17 @@ module.exports = {
     "eslint:recommended",
     "plugin:@typescript-eslint/recommended",
     "plugin:react/recommended",
+    "plugin:react/jsx-runtime",
+    "plugin:react-hooks/recommended",
     "prettier",
   ],
   parser: "@typescript-eslint/parser",
   parserOptions: {
     ecmaVersion: "latest",
     sourceType: "module",
+    ecmaFeatures: {
+      jsx: true,
+    },
   },
-  plugins: ["@typescript-eslint", "react"],
-  rules: {
-    "react/react-in-jsx-scope": "off",
-  },
+  plugins: ["@typescript-eslint", "react", "react-hooks"],
 };
