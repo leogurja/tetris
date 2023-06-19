b054287b2272fcba5cbdd203abc7410e3effc57f
Leonardo Gurgel <leo@gurgel.io>
2023-06-19 16:52:03 -0300

    fix buttons alignment

--- src/components/GameControls.tsx
+++ src/components/GameControls.tsx
@@ -18,12 +18,12 @@ export function GameControls() {
   }, [isGameOver]);
 
   return (
-    <div className="flex flex-col justify-center gap-5">
+    <div className="flex flex-col justify-center gap-5 w-full">
       <span className="bg-neutral-600 p-2 rounded-md" onClick={playPause}>
-        <PlayPauseIcon className="h-6 aspect-square" />
+        <PlayPauseIcon className="h-6 w-full" />
       </span>
       <span className="bg-neutral-600 p-2 rounded-md" onClick={reset}>
-        <ArrowPathIcon className="h-6 aspect-square" />
+        <ArrowPathIcon className="h-6 w-full" />
       </span>
     </div>
   );
