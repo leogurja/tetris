8c497b5ee77a47b991d8082d2d46d7b578654b27
Leonardo Gurgel <leo@gurgel.io>
2023-07-04 18:53:45 -0300

    text-sm links

--- src/components/nav/ExternalLink.tsx
+++ src/components/nav/ExternalLink.tsx
@@ -12,7 +12,7 @@ export function ExternalLink({
   return (
     <a
       href={href}
-      className="flex items-center gap-0.5 opacity-70 hover:opacity-100 transition-opacity"
+      className="flex items-center text-sm gap-0.5 opacity-70 hover:opacity-100 transition-opacity"
       rel="noreferrer"
       target="_blank"
     >
