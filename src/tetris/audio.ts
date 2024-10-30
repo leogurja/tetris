32c0359f71b5e09ed4457e3908a895860cfc583d
Leonardo Gurgel <leo@gurgel.io>
2024-10-30 18:44:04 -0300

    adjust default volumes

--- src/tetris/audio.ts
+++ src/tetris/audio.ts
@@ -1,27 +1,26 @@
 export enum Sfx {
-	GameOver = "game-over",
-	Click = "click",
-	Clear = "clear",
-	Drop = "drop",
-	LevelUp = "level-up",
+  GameOver = "game-over",
+  Click = "click",
+  Clear = "clear",
+  Drop = "drop",
+  LevelUp = "level-up",
 }
 
 export const defaultVolumes = {
-	"game-over": 0.1,
-	click: 0.4,
-	clear: 0.15,
-	drop: 0.3,
-	"level-up": 0.2,
-	korobeiniki: 0.1,
+  "game-over": 0.05,
+  click: 0.25,
+  clear: 0.08,
+  drop: 0.15,
+  "level-up": 0.15,
+  korobeiniki: 0.05,
 } as const;
 
 export const music = document.getElementById("korobeiniki") as HTMLAudioElement;
 
 export const play = (audio: Sfx) => {
-	// biome-ignore lint/style/noNonNullAssertion: can't be null if it's an AudioFile
-	const clone = document
-		.getElementById(audio)!
-		.cloneNode(true) as HTMLAudioElement;
-	clone.volume = defaultVolumes[audio];
-	clone.play();
+  const clone = document
+    .getElementById(audio)
+    ?.cloneNode(true) as HTMLAudioElement;
+  clone.volume = defaultVolumes[audio];
+  void clone.play();
 };
