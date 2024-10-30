fd164f718067bef8038bb00aa437fee19ed3b02d
Leonardo Gurgel <leo@gurgel.io>
2024-10-30 18:45:18 -0300

    accelerate music based on level

--- src/components/Game.tsx
+++ src/components/Game.tsx
@@ -1,5 +1,4 @@
 import { useEffect } from "react";
-import useInterval from "../hooks/useInterval";
 import useTetris from "../tetris";
 import { Sfx, defaultVolumes, music, play } from "../tetris/audio";
 import GameState from "../tetris/gameState";
@@ -9,42 +8,50 @@ import Controls from "./controls";
 import Menu from "./menu";
 
 export default function Game() {
-	const [update, tickRate, gameState, isMuted, level] = useTetris((t) => [
-		t.update,
-		t.tickRate,
-		t.gameState,
-		t.isMuted,
-		t.level(),
-	]);
+  const [update, tickRate, gameState, isMuted, level] = useTetris((t) => [
+    t.update,
+    t.tickRate(),
+    t.gameState,
+    t.isMuted,
+    t.level(),
+  ]);
 
-	useInterval(update, tickRate());
+  useEffect(() => {
+    if (tickRate <= 0) return;
 
-	// music
-	useEffect(() => {
-		if (gameState === GameState.Playing && !isMuted) {
-			music.volume = defaultVolumes.korobeiniki;
-			music.play();
-		} else {
-			music.pause();
-		}
-	}, [gameState, isMuted]);
+    const intervalId = setInterval(update, tickRate);
+    music.playbackRate = 1 + level / 20;
+    return () => {
+      clearInterval(intervalId);
+    };
+  }, [tickRate]);
 
-	// level up sound
-	useEffect(() => {
-		if (level === 0) return;
-		play(Sfx.LevelUp);
-	}, [level]);
+  // music
+  useEffect(() => {
+    if (gameState === GameState.Playing && !isMuted) {
+      music.volume = defaultVolumes.korobeiniki;
+      void music.play();
+    } else {
+      music.pause();
+    }
+  }, [gameState, isMuted]);
 
-	return (
-		<>
-			<main className="aspect-[9/16] max-w-full h-full flex flex-col items-center p-2">
-				<Statistics />
-				<section className="flex h-full w-full justify-center">
-					<Board />
-					<Menu />
-				</section>
-				<Controls />
-			</main>
-		</>
-	);
+  // level up sound
+  useEffect(() => {
+    if (level === 0) return;
+    play(Sfx.LevelUp);
+  }, [level]);
+
+  return (
+    <>
+      <main className="aspect-[9/16] max-w-full h-full flex flex-col items-center p-2">
+        <Statistics />
+        <section className="flex h-full w-full justify-center">
+          <Board />
+          <Menu />
+        </section>
+        <Controls />
+      </main>
+    </>
+  );
 }
